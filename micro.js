2beb81d675e745204c3c65845ffe5e3f490a9b65
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-06-28 16:03:43 -0400

    fade properly

--- micro.js
+++ micro.js
@@ -7,5 +7,5 @@ window.onload = async ()=>{
     await document.fonts.ready;
     await Micro.utils.awaitCallback($('#enterlogo'), $.fn.animate, {height: "200vh", padding: "20vh"}, 1500);
     await Micro.screens.switch('mainmenu', 500);
-    await Micro.utils.awaitCallback($('#menubuttons'), $.fn.fadeIn, 500);
+    await Micro.utils.awaitCallback($('#menubuttons'), $.fn.animate, {opacity: 1}, 500);
 };
