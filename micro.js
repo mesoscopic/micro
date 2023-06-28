86ce87c30ebec957d72272d12344bbc584ea982c
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-06-28 15:47:12 -0400

    apparently fonts aren't loaded before onload

--- micro.js
+++ micro.js
@@ -4,6 +4,7 @@ globalThis.Micro = Micro;
 window.onload = async ()=>{
     await Micro.screens.switch('intro', 500);
     await Micro.utils.wait(1000);
+    await document.fonts.ready;
     await Micro.utils.awaitCallback($('#enterlogo'), $.fn.animate, {height: "200vh", padding: "20vh"}, 1500);
     await Micro.screens.switch('mainmenu', 500);
     await Micro.utils.awaitCallback($('#menubuttons'), $.fn.fadeIn, 500);
