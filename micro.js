e67e269164570d9846e36c3afb2210a1bf8c1a05
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-06-28 15:43:18 -0400

    switch to onload so fonts load

--- micro.js
+++ micro.js
@@ -1,10 +1,10 @@
 import * as Micro from './engine/main.mjs'
 globalThis.Micro = Micro;
 
-$(async ()=>{
+window.onload = async ()=>{
     await Micro.screens.switch('intro', 500);
     await Micro.utils.wait(1000);
     await Micro.utils.awaitCallback($('#enterlogo'), $.fn.animate, {height: "200vh", padding: "20vh"}, 1500);
     await Micro.screens.switch('mainmenu', 500);
     await Micro.utils.awaitCallback($('#menubuttons'), $.fn.fadeIn, 500);
-});
+};
