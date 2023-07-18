a8fb75ba0d9af647b5e97e6988df47072d065ec4
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-07-17 21:02:20 -0400

    we are now rendering

--- engine/render.mjs
+++ engine/render.mjs
@@ -11,7 +11,13 @@ const render = {
     frame: function(time){
         if(render.lastFrame) render.fps = 1000/(time - render.lastFrame)
         render.lastFrame = time;
-        $('#render').attr("height", window.innerHeight).attr("width", window.innerWidth);
+        render.width = window.innerWidth;
+        render.height = window.innerHeight;
+        $('#render').attr("height", render.width).attr("width", render.width);
+
+        render.canvas.clearRect(0, 0, render.width, render.height);
+        render.canvas.fillText("i promise stuff is happening here", render.width/2-render.canvas.measureText("i promise stuff is happening here")/2, render.height/2);
+
         if(render.active) requestAnimationFrame(render.frame);
     }
 }
