b773001317127366d90bc516a838b038db2d1729
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-08-07 20:47:39 -0400

    fixed bugs with rendering

--- engine/render.mjs
+++ engine/render.mjs
@@ -1,6 +1,6 @@
 const render = {
     active: false,
-    scale: 1,
+    scale: 10,
     init: function(){
         this.active = true;
         this.canvas = $('#render').get(0).getContext('2d');
@@ -18,10 +18,10 @@ const render = {
 
         render.canvas.clearRect(0, 0, render.width, render.height);
         function renderChar(char, pos, size, opacity){
-            render.canvas.font = `${size*scale}px 'sharetechmono', 'unicodemono', monospace`;
+            render.canvas.font = `${size*render.scale}px 'sharetechmono', 'unicodemono', monospace`;
             render.canvas.textAlign = "center";
             render.canvas.textBaseline = "middle";
-            render.canvas.fillStyle = "#000000"+(opacity*255).toString(16);
+            render.canvas.fillStyle = "#000000"+Math.floor((opacity*255).toString(16));
             render.canvas.fillText(char, render.width/2 + pos[0], render.height/2 + pos[1]);
         }
         renderChar('◈', [20, 20], 1, 0.9);
