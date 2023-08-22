6f4d475ddae4bd410828f36cf95fb3060e4b244b
PegasusWebDev <121835328+PegasusWebDev@users.noreply.github.com>
2023-08-22 17:09:14 -0400

    :skull:

--- core/tile.mjs
+++ core/tile.mjs
@@ -1,5 +1,4 @@
 /* This module will define Tiles, both foreground and background characters that make up Micro's world. */
-import tileDictionary from './tiletypes.mjs'
 
 class Tile extends Micro.render.Character {
     constructor(char, pos, size, layer){
