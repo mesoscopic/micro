ffc454e819f037cd6a50939d481a9effbdacfdf5
mesoscopic <121835328+mesoscopic@users.noreply.github.com>
2025-02-14 13:18:22 -0500

    added spores

---
+++ godot/scripts/global_particles.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _update_amounts() -> void:
+	var spawn_distance: float = ceil(global_position.length() / 20.)
+	if spawn_distance < 30.:
+		$Spores.emitting = false
+	else:
+		$Spores.amount_ratio = 0.8**(0.02*(spawn_distance-30))
+		$Spores.rotation = get_angle_to(Vector2.ZERO)
+		$Spores.emitting = true
--- godot/scripts/ui/debug.gd
+++ godot/scripts/ui/debug.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _process(delta):
 	$BottomLeft/FPS.text = "FPS: %s" % int(1/delta);
+	if Micro.player:
+		$BottomLeft/Pos.text = "Position: %s" % round(Micro.player.global_position)
+		$BottomLeft/Tile.text = "Tile: %s" % round(Micro.player.global_position / 20.)
+		var spores = Micro.player.get_node("Camera/GlobalParticles/Spores")
+		$BottomRight/Spores.text = "Spores %s @ %s" % ["ON" if spores.emitting else "OFF", spores.amount_ratio]
