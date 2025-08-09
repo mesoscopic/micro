0a86c5177b836e1fc363bf665d99eb482e115c97
mesoscopic <121835328+mesoscopic@users.noreply.github.com>
2025-08-09 16:15:46 -0400

    feat: add gate tiles that block bullets and enemies

--- godot/editor/pattern_maker.gd
+++ godot/editor/pattern_maker.gd
@@ -6,3 +6,7 @@ extends TileMapLayer
 @export_tool_button("Save as TileMapPattern", "TileSet") var save = func ():
 	var pattern: TileMapPattern = get_pattern(get_used_cells())
 	ResourceSaver.save(pattern, "res://world/patterns/%s.tres" % pattern_name)
+
+@export_tool_button("Load TileMapPattern", "TileSet") var load = func ():
+	var pattern: TileMapPattern = ResourceLoader.load("res://world/patterns/%s.tres" % pattern_name, "TileMapPattern")
+	set_pattern(Vector2i.ZERO, pattern)
