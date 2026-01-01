80106ddbf27642f0f1effdc5eb138e9672e50ee6
mesoscopic <121835328+mesoscopic@users.noreply.github.com>
2025-12-31 19:57:54 -0500

    tweak: wrap setting focus

--- godot/ui/settings_menu.gd
+++ godot/ui/settings_menu.gd
@@ -1,19 +1,23 @@
 extends MarginContainer
 
 var focused_setting: Control
-@onready var setting_count := $Panel/Margin/MainBox/Options.get_child_count()
+@onready var settings_list := $Panel/Margin/MainBox/Options
+@onready var item_count := settings_list.get_child_count()
 var section_indices: Array[int] = []
 
 signal done
 
 func _ready() -> void:
-	for setting_button in $Panel/Margin/MainBox/Options.get_children():
+	for setting_button in settings_list.get_children():
 		setting_button.show_description.connect(show_description)
 		setting_button.focus_entered.connect(func (): focused_setting = setting_button)
-	$Panel/Margin/MainBox/Options.get_child(1).grab_focus()
-	for i in setting_count:
-		if $Panel/Margin/MainBox/Options.get_child(i) is SettingSection:
+	settings_list.get_child(1).grab_focus()
+	settings_list.get_child(1).focus_neighbor_top = settings_list.get_child(item_count-1).get_path()
+	settings_list.get_child(item_count-1).focus_neighbor_bottom = settings_list.get_child(1).get_path()
+	for i in item_count:
+		if settings_list.get_child(i) is SettingSection:
 			section_indices.append(i)
+	
 
 func _input(event) -> void:
 	if event.is_action_pressed("open_menu_key") or event.is_action_pressed("open_menu_joy") or event.is_action_pressed("ui_cancel"):
@@ -22,13 +26,13 @@ func _input(event) -> void:
 	if event.is_action_pressed("ui_next_section", true):
 		var i: int = focused_setting.get_index()
 		while i not in section_indices:
-			i = wrap(i+1, 0, setting_count)
-		$Panel/Margin/MainBox/Options.get_child(i+1).grab_focus()
+			i = wrap(i+1, 0, item_count)
+		settings_list.get_child(i+1).grab_focus()
 	if event.is_action_pressed("ui_prev_section", true):
-		var i: int = wrap(focused_setting.get_index() - 2, 0, setting_count)
+		var i: int = wrap(focused_setting.get_index() - 2, 0, item_count)
 		while i not in section_indices:
-			i = wrap(i-1, 0, setting_count)
-		$Panel/Margin/MainBox/Options.get_child(i+1).grab_focus()
+			i = wrap(i-1, 0, item_count)
+		settings_list.get_child(i+1).grab_focus()
 
 func show_description(description: String):
 	$Panel/Margin/MainBox/Info/Margin/Description.text = description
