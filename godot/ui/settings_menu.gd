e28b276fe706bf9b09afd97228af46e77fe8b074
mesoscopic <121835328+mesoscopic@users.noreply.github.com>
2026-01-06 19:02:52 -0500

    feat: settings menu scroll

--- godot/ui/settings_menu.gd
+++ godot/ui/settings_menu.gd
@@ -1,16 +1,19 @@
 extends MarginContainer
 
 var focused_setting: Control
-@onready var settings_list := $Panel/Margin/MainBox/Options
+@onready var scroll := $Panel/Margin/MainBox/Scroll
+@onready var settings_list := $Panel/Margin/MainBox/Scroll/Options
 @onready var item_count := settings_list.get_child_count()
 var section_indices: Array[int] = []
+
+const scroll_margin := 125
 
 signal done
 
 func _ready() -> void:
 	for setting_button in settings_list.get_children():
 		setting_button.show_description.connect(show_description)
-		setting_button.focus_entered.connect(func (): focused_setting = setting_button)
+		setting_button.focus_entered.connect(func (): focus(setting_button))
 	settings_list.get_child(1).grab_focus()
 	settings_list.get_child(1).focus_neighbor_top = settings_list.get_child(item_count-1).get_path()
 	settings_list.get_child(item_count-1).focus_neighbor_bottom = settings_list.get_child(1).get_path()
@@ -36,3 +39,10 @@ func _input(event) -> void:
 
 func show_description(description: String):
 	$Panel/Margin/MainBox/Info/Margin/Description.text = description
+
+func focus(setting: Control):
+	focused_setting = setting
+	if setting.position.y - scroll_margin <= scroll.scroll_vertical:
+		scroll.scroll_vertical = clamp(setting.position.y - scroll_margin, 0, scroll.size.y)
+	elif setting.position.y + scroll_margin >= scroll.scroll_vertical + scroll.size.y:
+		scroll.scroll_vertical = clamp(setting.position.y + scroll_margin - scroll.size.y, 0, scroll.size.y)
